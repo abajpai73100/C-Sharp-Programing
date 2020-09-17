166d59c1b6316085f340e0205e788ae4d7d4ad13
Anshul Bajpai <65597304+abajpai73100@users.noreply.github.com>
2020-09-17 13:30:20 +0530

    Add files via upload

---
+++ Assignment-1/reverseorder/reverseorder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseorder
+{
+    class Reverse
+
+    {
+        int numRev(int num)
+        {
+            int r =0;
+            while(num>0)
+            {
+                r = r * 10 + (num % 10);
+                num /= 10;
+            }
+
+            return r;
+        }
+       
+        static void Main(string[] args)
+        {
+            int num;
+         
+            Console.Write("Enter a number :");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Reverse rev = new Reverse();
+            Console.Write("Reverse is :" + rev.numRev(num));
+            Console.Read();
+
+        }
+    }
+}
