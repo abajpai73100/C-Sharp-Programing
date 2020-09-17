9affdb147ea20c222e824f1703aa353446bca02f
Anshul Bajpai <65597304+abajpai73100@users.noreply.github.com>
2020-09-17 13:36:22 +0530

    Add files via upload

---
+++ Assignment-1/table/table/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace table
+{
+    class Program
+    {
+        void table(int num,int limit)
+        {
+            for(int i=1;i<=limit;i++)
+            {
+                Console.WriteLine(num + "*" + i + " = "+num *i);
+
+            }
+            Console.WriteLine("Press any key and exit:");
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            int num, limit;
+
+            Console.Title = "Table";
+            Console.Write("Enter a number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a limit");
+            limit = Convert.ToInt32(Console.ReadLine());
+
+            Program pr = new Program();
+            
+            pr.table(num, limit);
+
+        }
+    }
+}
