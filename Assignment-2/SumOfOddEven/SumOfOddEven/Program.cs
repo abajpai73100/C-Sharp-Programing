39d6c1194c815b8b7532ea347d1e95d7d4c9772e
Anshul Bajpai <65597304+abajpai73100@users.noreply.github.com>
2020-09-29 10:35:14 +0530

    Add files via upload

---
+++ Assignment-2/SumOfOddEven/SumOfOddEven/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfOddEven
+{
+    class SumOfOddEven
+    {
+
+        int sumOdd()
+        {
+            int s = 0;
+            for (int i = 1; i <= 100; i += 2)
+            {
+                s += i;
+            }
+
+            return s;
+        }
+        int sumEven()
+        {
+            int s = 0;
+            for (int i = 2; i <= 100; i += 2)
+            {
+                s += i;
+            }
+
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            SumOfOddEven sm = new SumOfOddEven();
+
+            Console.Write("\nSum of odd numbers from 1 to 100 is: " + sm.sumOdd());
+            Console.Write("\nSum of even numbers from 1 to 100 is: " + sm.sumEven());
+
+            Console.ReadLine();
+        }
+    }
+}
