7c4ddb2d2e83361dabe6374a4075fd791596bcc3
Anshul Bajpai <65597304+abajpai73100@users.noreply.github.com>
2020-09-29 10:38:51 +0530

    Add files via upload

---
+++ Assignment-3/DoubleAddition/DoubleAddition/Program.cs
@@ -0,0 +1,29 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleAddition
+{
+    class DoubleAddition
+    {
+        double add(double a, double b)
+        {
+            return (a + b);
+        }
+        static void Main(string[] args)
+        {
+            var da = new DoubleAddition();
+
+            double a, b;
+            Console.Write("Enter two numbers: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("\n" + a + " + " + b + " = " + da.add(a, b));
+
+            Console.ReadLine();
+        }
+    }
+}
