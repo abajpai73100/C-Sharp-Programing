79d36e35c80144cffa8f5da3bed5dc9b40c64e40
Anshul Bajpai <65597304+abajpai73100@users.noreply.github.com>
2020-09-29 10:41:25 +0530

    Add files via upload

---
+++ Assignment-3/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Palindrome
+    {
+        int rev(int n)
+        {
+            int r = 0;
+            while (n != 0)
+            {
+                r = r * 10 + n % 10;
+                n /= 10;
+            }
+            return r;
+        }
+        static void Main(string[] args)
+        {
+            var pl = new Palindrome();
+
+            int n;
+
+            Console.Write("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == pl.rev(n))
+                Console.Write("\n\tPalindrome");
+            else
+                Console.Write("\n\tNot palindome");
+
+            Console.ReadLine();
+        }
+    }
+}
