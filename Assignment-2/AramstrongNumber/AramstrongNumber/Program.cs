228ea3bc9f8a3142d19ae66bbd6c09e47355cf82
Anshul Bajpai <65597304+abajpai73100@users.noreply.github.com>
2020-09-29 10:24:15 +0530

    Add files via upload

---
+++ Assignment-2/AramstrongNumber/AramstrongNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AramstrongNumber
+{
+    class ArmstrongNumber
+    {
+
+        Boolean check(int n)
+        {
+            int t, r, c = 0;
+            t = n;
+
+            while (t != 0)
+            {
+                r = t % 10;
+                c += (r * r * r);
+                t /= 10;
+            }
+
+            if (c == n)
+                return true;
+            else
+                return false;
+
+        }
+        static void Main(string[] args)
+        {
+            ArmstrongNumber an = new ArmstrongNumber();
+
+            int n;
+
+            Console.Write("Input a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (an.check(n))
+                Console.WriteLine("\nArmstrong number");
+            else
+                Console.WriteLine("\nNot Armstrong number");
+
+            Console.ReadLine();
+        }
+    }
+}
