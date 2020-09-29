467ae2f271b1c7c9e6504df9b3183532653ce491
Anshul Bajpai <65597304+abajpai73100@users.noreply.github.com>
2020-09-29 10:29:56 +0530

    Add files via upload

---
+++ Assignment-2/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i;
+            Console.Write(n1 + " " + n2 + " ");
+            for(i = 2; i <=8; ++i)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+
+            }
+            Console.Read();
+        }
+    }
+}
