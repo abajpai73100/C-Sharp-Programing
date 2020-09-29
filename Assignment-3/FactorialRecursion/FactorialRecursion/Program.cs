6e5f33cab8f5e357ea7a0fe50a443944bf88cfa1
Anshul Bajpai <65597304+abajpai73100@users.noreply.github.com>
2020-09-29 10:39:42 +0530

    Add files via upload

---
+++ Assignment-3/FactorialRecursion/FactorialRecursion/Program.cs
@@ -0,0 +1,30 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialRecursion
+{
+    class FactorialRecursion
+    {
+        double factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return (factorial(n - 1) * n);
+        }
+        static void Main(string[] args)
+        {
+            var fr = new FactorialRecursion();
+            int n;
+            Console.Write("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nFactorial is: " + fr.factorial(n));
+
+            Console.ReadLine();
+        }
+    }
+}
