7a23ca173ac19d16208af9af6456fa92fe6fb02c
Anshul Bajpai <65597304+abajpai73100@users.noreply.github.com>
2020-09-29 10:37:07 +0530

    Add files via upload

---
+++ Assignment-2/Temperatureconversion/Temperatureconversion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatureconversion
+{
+    class TemperatureConversion
+
+    {
+        float convert(float tmp)
+        {
+            return (tmp * 9) / 5 + 32;
+
+        }
+        static void Main(string[] args)
+        {
+            TemperatureConversion tc = new TemperatureConversion();
+
+            float tmp;
+
+            Console.Write("Enter temperature in celcius: ");
+            tmp = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("\n" + tmp + "C in Fahrenheit is: " + tc.convert(tmp) + "F");
+            Console.ReadLine();
+        }
+    }
+}
