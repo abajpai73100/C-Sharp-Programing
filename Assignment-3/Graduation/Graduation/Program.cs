36b5e48a4b89f83cc4e18948ff48d69d0e3bda50
Anshul Bajpai <65597304+abajpai73100@users.noreply.github.com>
2020-09-29 10:41:21 +0530

    Add files via upload

---
+++ Assignment-3/Graduation/Graduation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graduation
+{
+    class Graduation
+    {
+        Boolean calc(float mm, float gp, float ip, float hp)
+        {
+            if (mm < 55 && gp < 65)
+                return false;
+            else if ((mm >= 55 || gp >= 65) && (ip >= 60) && (gp >= 60))
+                return true;
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            var grd = new Graduation();
+
+            float mm, gp, ip, hp;
+
+            Console.Write("Enter marks in mathematics (graduation): ");
+            mm = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Enter percentage in graduation: ");
+            gp = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Enter percentage in intermediate: ");
+            ip = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Enter percentage in highschool: ");
+            hp = Convert.ToSingle(Console.ReadLine());
+
+            if (grd.calc(mm, gp, ip, hp))
+                Console.Write("\n\tEligibility criteria met.");
+            else
+                Console.Write("\n\tEligibility criteria not met.");
+
+            Console.ReadLine();
+        }
+    }
+}
