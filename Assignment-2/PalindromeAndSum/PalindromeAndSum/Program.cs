c997636a9aba541ae00b381113b26b4e47f39d95
Anshul Bajpai <65597304+abajpai73100@users.noreply.github.com>
2020-09-29 10:31:25 +0530

    Add files via upload

---
+++ Assignment-2/PalindromeAndSum/PalindromeAndSum/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeAndSum
+{
+    class PalindromeAndSum
+    {
+        void palinAndSum(int n)
+        {
+            int t, p, r = 0, s = 0;
+            t = n;
+            p = n;
+
+            while (n != 0)
+            {
+                r = r * 10 + (n % 10);
+                n /= 10;
+            }
+
+            while (p > 10)
+            {
+                s += p % 10;
+                p /= 10;
+            }
+            s += p;
+
+            if (t == r)
+                Console.WriteLine("\n\tPalindrome number\n");
+            else
+                Console.WriteLine("\nNot a Palindrome number\n");
+
+            if (t == s)
+                Console.WriteLine("\tSum of digits(" + s + ") " + "is equal to the number (" + t + ")\n");
+            else
+                Console.WriteLine("\tSum of digits(" + s + ") " + "is not equal to the number (" + t + ")\n");
+        }
+        static void Main(string[] args)
+        {
+            PalindromeAndSum ps = new PalindromeAndSum();
+
+            int n;
+
+            Console.Write("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            ps.palinAndSum(n);
+
+            Console.ReadLine();
+        }
+    }
+}
